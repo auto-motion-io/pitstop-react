fbd43a93ee719ce332cf5c4a9f49c84cb0bf775e
thaisanbrg <thaisanbrg@gmail.com>
2024-05-08 20:11:48 -0300

    alteracoes login

--- src/pages/loginPitstop/LoginPitstop.jsx
+++ src/pages/loginPitstop/LoginPitstop.jsx
@@ -11,7 +11,7 @@ const LoginPitstop = () => {
 
       <div >
         <img style={{ position: "absolute", zIndex: "-1", right: "0", marginTop: "-20vh", bottom: 0, width: "70%" }} src={pista} alt="" />
-        <ModalLogin logo={logoPitstop} nome1={"E-mail*"} nome2={"Senha"} tamanho1={"93%"} tamanho2={"93%"} tamanhoFundo1={"80%"} tamanhoFundo2={"80%"} cor={"#C66D2C"} />
+        <ModalLogin logo={logoPitstop} nome1={"E-mail*"} nome2={"Senha"} tamanho1={"97%"} tamanho2={"97%"} tamanhoFundo1={"80%"} tamanhoFundo2={"80%"} cor={"#C66D2C"} />
 
         <div style={{ marginTop: "3vh" }}>
           <a id={style.textinho} style={{ marginLeft: "10vw" }} href="#">Ainda não tem login? Cadastre-se no PitStop </a>
