83ef785f57dddf3d1b7d1e73b78dc549ad25b8f0
Davidnms <david.msilva@sptech.school>
2024-05-09 16:53:47 -0300

    Ajustando Input

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,21 +1,12 @@
 import React from "react";
 import style from "./Input.module.css";
 
-const Input = ({ nome, type = "text", tamanho, tamanhoFundo, onInput = null, onChange, maxLength = 255, value = "", altura = "36px", imagem = null }) => {
-  let inputImage = "";
-  if (image != null) {
-    inputImage =
-      <div className={style["input-lupa"]}>
-        <div className={style["lupa"]}><img src={lupa} alt="Imagem de Lupa" /></div>
-        <input type="text" />
-      </div>
-  }
+const Input = ({ nome, type = "text", tamanho, tamanhoFundo, onInput = null, onChange, maxLength = 255, value = "", altura = "36px" }) => {
   return (
-    
-    <div id={style.backgroundInput} style={{ width: tamanhoFundo }}>
-      <span id={style.span}>{nome} </span>
-      <input required type={type} id={style.input} value={value} maxLength={maxLength} onInput={onInput} onChange={onChange} style={{ width: tamanho, height: altura }} />
-    </div>
+      <div id={style.backgroundInput} style={{width: tamanhoFundo}}>
+        <span id={style.span}>{nome} </span>
+        <input required type={type} id={style.input} value={value} maxLength={maxLength} onInput={onInput} onChange={onChange} style={{width: tamanho, height: altura}}/>
+      </div>
 
   );
 };
